e97a7beaf3aaa894d6a04bfa30c7e024d97a1e39
Michael Hatherly <michaelhatherly@gmail.com>
2014-09-29 21:48:52 +0200

    Script to check for breakages caused by Docile changes.

---
+++ test/package-checks.jl
@@ -0,0 +1,16 @@
+# Try to load packages that rely on Docile so then don't get broken.
+
+const PACKAGES = [
+    "HttpServer"
+    ]
+
+for package in PACKAGES
+    file = joinpath(Pkg.dir(package), "src", package * ".jl")
+    try
+        info("Trying $(package)...")
+        include(file)
+    catch err
+        warn("Failed $(package).")
+        println(err)
+    end
+end
