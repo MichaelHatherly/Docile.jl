5e8e80c01b972fe07a521b40155ec4b83de159b2
Michael Hatherly <michaelhatherly@gmail.com>
2015-04-04 22:31:44 +0200

    Update build script message.

--- docs/build.jl
+++ docs/build.jl
@@ -31,6 +31,11 @@ cd(dirname(@__FILE__)) do
         print(f, """
         Files in this directory are generated using the `build.jl` script. Make
         all changes to the originating docstrings/files rather than these ones.
+
+        Documentation should *only* be build directly on the `master` branch.
+        Source links would otherwise become unavailable should a branch be
+        deleted from the `origin`. This means potential pull request authors
+        *should not* run the build script when filing a PR.
         """)
     end
 
