24b60407011226830de9b52e75c66db704bc6f33
peter1000 <https://github.com/peter1000>
2015-05-10 16:54:53 -0300

    not to be merged only to run Windows tests
    try unicode not to be merged only for the Windows problem
    
    Anyway the MetaSyntax tests need to be re-done once the error checks for
    `meta within meta` are in place: !!meta(some text an !!innermeta(Test))
    should raise an error
    https://github.com/MichaelHatherly/Docile.jl/issues/102#issuecomment-100670793

---
+++ test/Formats/MetadataSyntax.jl
@@ -0,0 +1,17 @@
+module MetadataSyntax
+
+# "example0: unicode metaname: !!笔者(所以不多说了)"
+# function example0() end
+
+"example1: unicode text: !!metaisnormal(Água é uma substância química cujas moléculas são formadas por dois átomos de hidrogênio e um de oxigênio.)"
+function example1() end
+
+"example2: unicode meta txt normal: !!moléculas(Just normal text.)"
+function example2() end
+
+"example3: unicode meta and txt: !!moléculas(Água é uma substância química cujas moléculas são formadas por dois átomos de hidrogênio e um de oxigênio.)"
+function example3() end
+
+
+
+end
