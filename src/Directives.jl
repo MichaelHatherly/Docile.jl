9b86bd580ad321be31aca42ca8528b35384e163c
Michael Hatherly <michaelhatherly@gmail.com>
2015-08-15 19:02:01 +0200

    Add some docs.

--- src/Directives.jl
+++ src/Directives.jl
@@ -5,7 +5,7 @@ module Directives
 
 using Base.Meta, ..Utilities
 
-export @directive_str, build, parsebracket
+export @directive_str, build, parsebracket, withdefault
 
 
 immutable Directive{name} end
@@ -16,6 +16,19 @@ const DIRECTIVE_MARKER = r"^(\w+):\s*(?s)(.*)"
 
 const DEFAULT = Ref{Directive}(directive"docs"())
 
+"""
+    withdefault(func, directive)
+
+> Set the default directive to use within the call to ``func``.
+
+**Usage:**
+
+```julia
+withdefault(:custom) do
+    # ...
+end
+```
+"""
 withdefault(func :: Function, directive :: Symbol) =
     @with DEFAULT.x = Directive{directive}() func()
 
@@ -37,6 +50,35 @@ function buildeach(s :: Str, out)
 end
 buildeach(x, out) = push!(out, x)
 
+"""
+    parsebracket(directive :: Directive, text)
+
+> User-extensible syntax hook.
+
+Extending this function allows for handling of ``{{custom:...}}`` syntax.
+
+**Example:**
+
+To define a directive that reverses the text it contains:
+
+```julia
+using Docile.Directives
+Directives.parsebracket(:: directive"reverse", text) = reverse(text)
+```
+
+Now when
+
+```md
+{{reverse:hello world}}
+```
+
+is parsed it will result in the following output:
+
+```md
+dlrow olleh
+```
+"""
+function parsebracket end
 
 parsebracket(text :: AbstractString) = parsebracket(getdirective(text)...)
 parsebracket{D}(:: Directive{D}, text) = error("Unknown directive: '$D'")
--- src/Docs.jl
+++ src/Docs.jl
@@ -4,9 +4,33 @@
 module Docs
 
 using ..Utilities
+using ..Directives
 
 const HOOKS = symbol("#HOOKS#")
 
+export addhook, directives
+
+"""
+    addhook(func)
+
+> Register a function to be called on each docstring prior to storing it.
+
+**Usage:**
+
+```julia
+using Docile.Docs
+
+reverser(str, def) = (reverse(str), def)
+
+addhook(custom)
+
+addhook() do str, def
+    # ...
+end
+```
+
+**Note:** registered hooks are only applied to docstrings written after the hook is added.
+"""
 function addhook(hook :: Function, curmod = current_module())
     isdefined(curmod, HOOKS) || eval(curmod, :(const $(HOOKS) = Function[]))
     push!(getfield(curmod, HOOKS), hook)
@@ -24,5 +48,29 @@ function __init__()
     # Hook into the Julia documentation system.
     Base.DocBootstrap.setexpand!(docm)
 end
+
+# Pre-made hooks.
+
+"""
+    directives(str, def)
+
+> Docsystem hook for executing ``{{...}}`` directives embedded in docstrings.
+
+**Usage:**
+
+```julia
+module MyModule
+
+using Docile.Docs
+
+addhook(directives)
+
+"{{...}}"
+f(x) = ...
+
+end
+```
+"""
+directives(str, def) = (build(:string, str), def)
 
 end
