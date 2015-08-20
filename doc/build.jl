e7e4a2c3912a32da599892f557cc571cfa4703fd
Michael Hatherly <michaelhatherly@gmail.com>
2015-08-20 07:49:37 +0200

    Remove old build script.

--- doc/build.jl
+++ doc/build.jl
@@ -1,14 +1,3 @@
-# TODO: generalise this as a function in Lexicon.
+using Docile
 
-using Docile, Lexicon
-
-const source = "src"
-const build  = "build"
-
-const input  = [joinpath(source, f) for f in readdir(source)]
-const output = [joinpath(build, f)  for f in readdir(source)]
-
-for (i, o) in zip(input, output)
-    info("Writing: ", i, " to ", o)
-    Lexicon.Generate.loadfile(i, o)
-end
+makedocs()
