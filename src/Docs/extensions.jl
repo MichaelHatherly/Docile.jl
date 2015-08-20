9735205066e082ef30317c9c8534cf33f6e73524
Michael Hatherly <michaelhatherly@gmail.com>
2015-08-20 08:30:44 +0200

    Add 'docmeta' hook.

--- src/Docs/extensions.jl
+++ src/Docs/extensions.jl
@@ -49,3 +49,59 @@ function vecdoc(str, def)
     isexpr(def, :vect) || return str, def
     nothing, Expr(:block, [:(@doc($(esc(str)), $(esc(x)))) for x in def.args]...)
 end
+
+# Expr(:meta, :doc, ...) implementation.
+
+"""
+    docmeta(str, def)
+
+Choose expression to document in macro-generated code.
+
+**Usage:**
+
+```julia
+using Docile
+addhook(Docile.Docs.docmeta)
+
+macro macroname(def)
+    ex = expression_to_document(def)
+    quote
+        # ...
+        Expr(:meta, :doc, esc(ex))
+        # ...
+    end
+end
+
+"..."
+@macroname begin
+    # ...
+end
+```
+"""
+function docmeta(str, def)
+    if isexpr(def, :macrocall)
+        def = macroexpand(def)
+        replacemeta!(def, str) && return nothing, esc(def)
+    end
+    str, def
+end
+
+"""
+    replacemeta!(ex, str)
+
+Walk over an expression ``ex`` and replace the first ``Expr(:meta, :doc, ...)``
+node found with a ``@doc`` call.
+"""
+function replacemeta!(ex :: Expr, str)
+    if isexpr(ex, :meta) && ex.args[1] == :doc
+        ex.head = :macrocall
+        ex.args = [symbol("@doc"), str, ex.args[end]]
+        return true
+    else
+        for each in ex.args
+            replacemeta!(each, str) && return true
+        end
+    end
+    false
+end
+replacemeta!(other, str) = false
